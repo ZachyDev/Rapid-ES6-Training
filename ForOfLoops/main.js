60b8ab5e81da3da9e14879a30e58cef622d9f12c
ZachyDev <mosetizachary001@gmail.com>
2020-06-16 22:03:54 +0300

    For..of..Loops

---
+++ ForOfLoops/main.js
@@ -0,0 +1,11 @@
+const studentList = ['Moseti','Mary','Doroh'];
+for(let student of studentList ){
+    console.log(student);
+}
+// iterating through a string
+const fname = 'Zachary';
+let count =  0;
+for(char in fname) {
+    count++;
+}
+console.log(count)
