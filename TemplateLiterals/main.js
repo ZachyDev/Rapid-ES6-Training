1a44f5189619741fa4b51fa6ec4643b5f5467768
ZachyDev <mosetizachary001@gmail.com>
2020-06-16 22:32:18 +0300

    Temlate Liretals

---
+++ TemplateLiterals/main.js
@@ -0,0 +1,14 @@
+const name = 'Zachary';
+console.log(`Hello ${ name }`);
+
+let developers = `
+    Moseti,
+    Rodgers,
+    Ann
+`;
+console.log(developers);
+
+const sayHello = (name) => {
+    console.log(name);
+}
+sayHello `Zachary`;
