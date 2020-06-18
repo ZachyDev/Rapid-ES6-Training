4cab29ccd779240da77e022c0ab2b1e4d0c954df
ZachyDev <mosetizachary001@gmail.com>
2020-06-19 00:36:13 +0300

    Symbols generate a unique id for each variable

--- Symbols/main.js
+++ Symbols/main.js
@@ -1,4 +1,16 @@
 // create a new symbol
 const eventSymbol = Symbol('resize the event');
 // console.log(typeof eventSymbol);
-console.log(eventSymbol.toString());
+// console.log(eventSymbol.toString()); human readble format
+
+ const CALCULATE_EVENT_SYMBOL = Symbol('Calculate event');
+//  console.log(CALCULATE_EVENT_SYMBOL.toString()); a Symbol can be assigned to const,let and var
+
+
+const s = Symbol('event');
+const s2 = Symbol('event');
+// console.log(s === s2); shows false,symbols create a unique id
+
+// built inn registry
+let s = Symbol.for('event');
+console.log(s.toString());
