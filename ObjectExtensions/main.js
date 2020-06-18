f2e6ff69a2206a1c45b28e05342d85fe34881c99
ZachyDev <mosetizachary001@gmail.com>
2020-06-19 01:18:41 +0300

    Updated Object.setProtypeOf

--- ObjectExtensions/main.js
+++ ObjectExtensions/main.js
@@ -14,7 +14,13 @@ let studentInfo = {
     [Symbol.for('age')]: 20,
     course: 'CS'
 }
-
-let studAge = studentInfo[Symbol.for('age')];
+let student1 = {
+    name: "Dorothy",
+    [Symbol.for('age')]: 21,
+    course2: "Software Engineering"
+}
+Object.setPrototypeOf(studentInfo,student1);
+console.log(studentInfo.course2);
+// let studAge = studentInfo[Symbol.for('age')];
 // console.log(studAge);
 // console.log(Object.getOwnPropertySymbols(studentInfo))
