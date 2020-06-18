c6acdc35f6706640587da9ec3957a0a34b174fee
ZachyDev <mosetizachary001@gmail.com>
2020-06-19 02:50:18 +0300

    isFinite() vs Number.isFinite()

--- NumberExtensions/main.js
+++ NumberExtensions/main.js
@@ -4,3 +4,7 @@ console.log(Number.parseFloat === parseFloat);
 const s = 'NaN';
 console.log(isNaN(s));
 console.log(Number.isNaN(s));
+
+const a = '8000';
+console.log(isFinite(a));
+console.log(Number.isFinite(a));
