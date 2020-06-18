d50605400d350e5f5d4c0584f0a7781fe3b919b9
ZachyDev <mosetizachary001@gmail.com>
2020-06-19 02:33:07 +0300

    Astral plane values continued

--- String-Extensions/main.js
+++ String-Extensions/main.js
@@ -9,4 +9,6 @@ console.log(message.endsWith('technologies'));
 console.log(message.includes('Za'));
 
 let title = "Zachy's \u{1f3c4} Blog";
+let surfer = "\u{1f3c4}";
 console.log(title);
+console.log(surfer.length);
