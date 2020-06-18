ecd5337d7851fe40109f2f46f9cd89ce858b7ee4
ZachyDev <mosetizachary001@gmail.com>
2020-06-19 02:53:13 +0300

    fixed an error

--- NumberExtensions/main.js
+++ NumberExtensions/main.js
@@ -1,13 +1,17 @@
 console.log(Number.parseInt === parseInt);
 console.log(Number.parseFloat === parseFloat);
 
+// isNaN
 const s = 'NaN';
 console.log(isNaN(s));
 console.log(Number.isNaN(s));
 
+// isFinite
 const a = '8000';
 console.log(isFinite(a));
 console.log(Number.isFinite(a));
 
+// isInteger
 let sum = 505.50;
 console.log(Number.isInteger(sum));
+
