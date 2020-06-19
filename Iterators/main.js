8336d5074442f1a8fa60dd8c0c905520b54b1fc5
ZachyDev <mosetizachary001@gmail.com>
2020-06-19 21:36:25 +0300

    Finished working with iterators - iterators are vital in looping through an array or array of objects

--- Iterators/main.js
+++ Iterators/main.js
@@ -2,7 +2,7 @@
 const  nums = [100,200,300];
 console.log(typeof nums[Symbol.iterator]);
 
-const it = nums[Symbol.iterator]()
+const it = nums[Symbol.iterator]();
 it.next();
 it.next();
 // it.next(); the iterator is now exhausted
