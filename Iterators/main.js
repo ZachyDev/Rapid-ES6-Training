a69f954e9d8c840c67917496b6a52c9a97d037e3
ZachyDev <mosetizachary001@gmail.com>
2020-06-19 20:21:29 +0300

    Appliied iterator function in objects

--- Iterators/main.js
+++ Iterators/main.js
@@ -1,9 +1,22 @@
-// iterators
-const  nums = [100,200,300];
-console.log(typeof nums[Symbol.iterator]);
+// // iterators
+// const  nums = [100,200,300];
+// console.log(typeof nums[Symbol.iterator]);
 
-const it = nums[Symbol.iterator]();
-it.next();
-it.next();
-// it.next(); the iterator is now exhausted
-console.log(it.next());
+// const it = nums[Symbol.iterator]();
+// it.next();
+// it.next();
+// // it.next(); the iterator is now exhausted
+// console.log(it.next());
+
+// Object of users 
+const users = [
+   'Zachy',
+   'Finna',
+   'Doroh'
+]
+
+const showAllUsers = (iter) => {
+   return iter.next();
+
+}
+console.log(showAllUsers(users[Symbol.iterator]()));
