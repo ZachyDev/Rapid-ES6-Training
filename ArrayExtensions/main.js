8389b48d372d46b42a38df89c9085e7267555b65
ZachyDev <mosetizachary001@gmail.com>
2020-06-20 15:02:43 +0300

    Array.from() - created a brand new array from the current array

--- ArrayExtensions/main.js
+++ ArrayExtensions/main.js
@@ -4,3 +4,16 @@
 // solution in es6
 let salary = Array.of(10000);
 console.log(salary.length);
+// example2
+let students = Array.of('Zachary','Doroh','Finna');
+console.log(students[0]);
+
+// Array.from demo  - Array.from creates a new array based on the current array
+
+let amounts = [1000,2000,3000];
+
+// let amountIncrease = Array.from(amounts, amount => amount + 100);
+let amountIncrease = Array.from(amounts, (amount) => {
+    return amount + 200;
+})
+console.log(amountIncrease)
