3aa860a12f4e377a42203acbb910dc7c3f3c49cd
ZachyDev <mosetizachary001@gmail.com>
2020-06-20 12:32:09 +0300

    Conditioned generators

--- Generators/main.js
+++ Generators/main.js
@@ -2,7 +2,20 @@ function *process() {
     yield 8000;
     yield 9000;
 }
-const it = process();
+const it = process(); //the result is an itertor
 it.next();
 it.next()
 console.log(it.next())
+
+// conditioned generator
+
+function *randomId() {
+    let nextId = 2000;
+    while(true) {
+        yield(nextId++);
+    }
+}
+const randomNumber = randomId();
+randomNumber.next();
+randomNumber.next();
+console.log(randomNumber.next());
